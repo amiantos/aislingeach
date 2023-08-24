082493e95b3bac3362c80ace56a2fcaa727abc2a
Brad Root <bradroot@me.com>
2023-08-23 18:32:44 -0700

    lay more groundwork for improved album views

--- Apple Platforms/iOS/Views/Image Browsers/Album View/AlbumCollectionViewCell.swift
+++ Apple Platforms/iOS/Views/Image Browsers/Album View/AlbumCollectionViewCell.swift
@@ -18,25 +18,35 @@ class AlbumCollectionViewCell: UICollectionViewCell {
     var viewPredicate: NSPredicate?
     var viewTitle: String?
 
-    func setup(count: String, predicate: NSPredicate, title: String, image: GeneratedImage?) {
+    func setup(predicate: NSPredicate, title: String) {
         cellBackgroundView.layer.cornerRadius = 8
         promptLabel.text = title
-        imageCountLabel.text = count
         viewPredicate = predicate
         viewTitle = title
         if title == "Favorites" {
             favoriteIcon.isHidden = false
         }
-        if let object = image {
-            DispatchQueue.main.async { [self] in
-                if let cachedImage = ImageCache.standard.getImage(key: NSString(string: object.uuid!.uuidString)) {
-                    Log.debug("Reloading cached UIImage...")
-                    imageView.image = cachedImage
-                } else {
-                    if let image = UIImage(data: object.image!) {
-                        imageView.image = image
-                        ImageCache.standard.cacheImage(image: image, key: NSString(string: object.uuid!.uuidString))
+    }
+
+    func loadData() {
+        Log.debug("Load data...")
+        Task {
+            if let predicate = self.viewPredicate {
+                let result = await ImageDatabase.standard.getCountAndRecentImageForPredicate(predicate: predicate)
+                self.imageCountLabel.text = result.0.formatted()
+                if let object = result.1 {
+                    if let cachedImage = ImageCache.standard.getImage(key: NSString(string: object.uuid!.uuidString)) {
+                        Log.debug("Reloading cached UIImage...")
+                        self.imageView.image = cachedImage
+                    } else {
+                        if let image = UIImage(data: object.image!) {
+                            self.imageView.image = image
+                            ImageCache.standard.cacheImage(image: image, key: NSString(string: object.uuid!.uuidString))
+                        }
                     }
+                }
+                DispatchQueue.main.async {
+                    self.layoutSubviews()
                 }
             }
         }
--- Apple Platforms/iOS/Views/Image Browsers/Album View/AlbumsCollectionViewController.swift
+++ Apple Platforms/iOS/Views/Image Browsers/Album View/AlbumsCollectionViewController.swift
@@ -12,16 +12,12 @@ import UIKit
 private let reuseIdentifier = "albumCell"
 
 class AlbumStruct {
-    let count: String
     let predicate: NSPredicate
     let title: String
-    let image: GeneratedImage?
 
-    init(count: String, predicate: NSPredicate, title: String, image: GeneratedImage?) {
-        self.count = count
+    init(predicate: NSPredicate, title: String) {
         self.predicate = predicate
         self.title = title
-        self.image = image
     }
 }
 
@@ -30,9 +26,7 @@ class AlbumsCollectionViewController: UICollectionViewController, UICollectionVi
 
     var isLoading: Bool = false
 
-    var presetAlbums: [AlbumStruct] = []
-    var promptAlbums: [AlbumStruct] = []
-    var smartAlbums: [AlbumStruct] = []
+    var albums: [AlbumStruct] = []
 
     var menuButton: UIBarButtonItem = .init()
 
@@ -47,10 +41,18 @@ class AlbumsCollectionViewController: UICollectionViewController, UICollectionVi
         } else {
             // setup menu
             menuButton = UIBarButtonItem(image: UIImage(systemName: "ellipsis.circle"), style: .plain, target: self, action: nil)
-            navigationItem.rightBarButtonItems = [menuButton]
-            menuButton.menu = UIMenu(children: [UIAction(title: "Show hidden gallery", image: UIImage(systemName: "eye.slash"), state: .off, handler: { [self] _ in
-                showHiddenGallery()
-            })])
+            let addAlbumButton = UIBarButtonItem(image: UIImage(systemName: "rectangle.stack.fill.badge.plus"), style: .plain, target: self, action: nil)
+            navigationItem.rightBarButtonItems = [addAlbumButton, menuButton]
+            menuButton.menu = UIMenu(children: [
+                UIAction(
+                    title: "Show hidden gallery",
+                    image: UIImage(systemName: "eye.slash"),
+                    state: .off,
+                    handler: { [self] _ in
+                        showHiddenGallery()
+                    }
+                )
+            ])
         }
 
         NotificationCenter.default.addObserver(self, selector: #selector(loadDataSource), name: .imageDatabaseUpdated, object: nil)
@@ -86,123 +88,91 @@ class AlbumsCollectionViewController: UICollectionViewController, UICollectionVi
 
         isLoading = true
 
-        presetAlbums = []
-        promptAlbums = []
-
-        Task {
-            let recentImagesPredicate = NSPredicate(format: "isHidden = %d", self.showHidden)
-            let recentImagesResult = await ImageDatabase.standard.getCountAndRecentImageForPredicate(predicate: recentImagesPredicate)
-            presetAlbums.append(
-                AlbumStruct(
-                    count: recentImagesResult.0.formatted(),
-                    predicate: recentImagesPredicate,
-                    title: "Recents",
-                    image: recentImagesResult.1
-                )
+        albums = [
+            AlbumStruct(
+                predicate: NSPredicate(format: "isHidden = %d", self.showHidden),
+                title: "Recents"
+            ),
+            AlbumStruct(
+                predicate: NSCompoundPredicate(andPredicateWithSubpredicates: [
+                    NSPredicate(format: "isFavorite = %d", true),
+                    NSPredicate(format: "isHidden = %d", self.showHidden)
+                ]),
+                title: "Favorites"
+            ),
+            AlbumStruct(
+                predicate: NSCompoundPredicate(andPredicateWithSubpredicates: [
+                    NSPredicate(format: "promptSimple CONTAINS %@", "Eyre"),
+                    NSPredicate(format: "isHidden = %d", self.showHidden)
+                ]),
+                title: "Eyre"
+            ),
+            AlbumStruct(
+                predicate:  NSCompoundPredicate(andPredicateWithSubpredicates: [
+                    NSPredicate(format: "promptSimple CONTAINS %@", "end of the world"),
+                    NSPredicate(format: "isHidden = %d", self.showHidden)
+                ]),
+                title: "end of the world"
+            ),
+            AlbumStruct(
+                predicate:  NSCompoundPredicate(andPredicateWithSubpredicates: [
+                    NSPredicate(format: "promptSimple CONTAINS %@", "Keith Haring"),
+                    NSPredicate(format: "isHidden = %d", self.showHidden)
+                ]),
+                title: "Keith Haring"
+            ),
+            AlbumStruct(
+                predicate:  NSCompoundPredicate(andPredicateWithSubpredicates: [
+                    NSPredicate(format: "promptSimple CONTAINS %@", "Christina Ricci"),
+                    NSPredicate(format: "isHidden = %d", self.showHidden)
+                ]),
+                title: "Christina Ricci"
+            ),
+            AlbumStruct(
+                predicate:  NSCompoundPredicate(andPredicateWithSubpredicates: [
+                    NSPredicate(format: "promptSimple CONTAINS %@", "bread"),
+                    NSPredicate(format: "isHidden = %d", self.showHidden)
+                ]),
+                title: "bread"
             )
+        ]
 
-            let favoriteImagesPredicate = NSCompoundPredicate(andPredicateWithSubpredicates: [
-                NSPredicate(format: "isFavorite = %d", true),
-                NSPredicate(format: "isHidden = %d", self.showHidden)
-            ])
-            let favoriteImagesResult = await ImageDatabase.standard.getCountAndRecentImageForPredicate(predicate: favoriteImagesPredicate)
-            presetAlbums.append(
-                AlbumStruct(
-                    count: favoriteImagesResult.0.formatted(),
-                    predicate: favoriteImagesPredicate,
-                    title: "Favorites",
-                    image: favoriteImagesResult.1
-                )
-            )
+        self.collectionView.reloadData()
+        self.isLoading = false
 
-            let smartAlbumPredicate = NSCompoundPredicate(andPredicateWithSubpredicates: [
-                NSPredicate(format: "promptSimple CONTAINS %@", "Eyre"),
-                NSPredicate(format: "isHidden = %d", self.showHidden)
-            ])
-            let smartAlbumResult = await ImageDatabase.standard.getCountAndRecentImageForPredicate(predicate: smartAlbumPredicate)
-            smartAlbums.append(AlbumStruct(count: smartAlbumResult.0.formatted(), predicate: smartAlbumPredicate, title: "Eyre", image: smartAlbumResult.1))
-
-            let smartAlbumPredicate2 = NSCompoundPredicate(andPredicateWithSubpredicates: [
-                NSPredicate(format: "promptSimple CONTAINS %@", "end of the world"),
-                NSPredicate(format: "isHidden = %d", self.showHidden)
-            ])
-            let smartAlbumResult2 = await ImageDatabase.standard.getCountAndRecentImageForPredicate(predicate: smartAlbumPredicate2)
-            smartAlbums.append(AlbumStruct(count: smartAlbumResult2.0.formatted(), predicate: smartAlbumPredicate2, title: "end of the world", image: smartAlbumResult2.1))
-
-            DispatchQueue.main.async {
-                self.collectionView.reloadData()
-                self.isLoading = false
-            }
-        }
     }
 
     // MARK: UICollectionViewDataSource
 
     override func numberOfSections(in _: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 2
+        return 1
     }
 
     override func collectionView(_: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        switch section {
-        case 0:
-            return presetAlbums.count
-        case 1:
-            return smartAlbums.count
-        default:
-            return 0
-        }
+        return albums.count
     }
 
     func collectionView(_ collectionView: UICollectionView, layout _: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-        if section == 0 {
-            return CGSize(width: 0, height: 0)
-        } else {
-            let indexPath = IndexPath(row: 0, section: section)
-            let headerView = self.collectionView(collectionView, viewForSupplementaryElementOfKind: UICollectionView.elementKindSectionHeader, at: indexPath)
-
-            // Use this view to calculate the optimal size based on the collection view's width
-            return headerView.systemLayoutSizeFitting(CGSize(width: collectionView.frame.width, height: UIView.layoutFittingExpandedSize.height),
-                                                      withHorizontalFittingPriority: .required, // Width is fixed
-                                                      verticalFittingPriority: .fittingSizeLevel) // Height can be as large as needed
-        }
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        if let sectionHeader = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "albumSectionTitle", for: indexPath) as? AlbumSectionTitleCollectionReusableView {
-            if indexPath.section == 0 {
-                return UICollectionReusableView()
-            }
-            switch indexPath.section {
-            case 1:
-                sectionHeader.sectionLabel.text = smartAlbums.count > 0 ? "Your Albums" : ""
-            default:
-                sectionHeader.sectionLabel.text = "Section \(indexPath.section)"
-            }
-
-            return sectionHeader
-        }
-        return UICollectionReusableView()
+        return CGSize(width: 0, height: 0)
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "albumCell", for: indexPath) as! AlbumCollectionViewCell
 
-        var album: AlbumStruct?
-        if indexPath.section == 0 {
-            album = presetAlbums[indexPath.row]
-        } else {
-            album = smartAlbums[indexPath.row]
-        }
-        guard let foundAlbum = album else { fatalError() }
-        cell.setup(count: foundAlbum.count, predicate: foundAlbum.predicate, title: foundAlbum.title, image: foundAlbum.image)
+        let foundAlbum = albums[indexPath.row]
+        cell.setup(predicate: foundAlbum.predicate, title: foundAlbum.title)
         return cell
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        guard let cell = cell as? AlbumCollectionViewCell else { return }
+        cell.loadData()
     }
 
     override func collectionView(_: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let controller = storyboard.instantiateViewController(withIdentifier: "imageGalleryView") as! ThumbnailBrowserViewController
-        let data = indexPath.section == 0 ? presetAlbums[indexPath.row] : smartAlbums[indexPath.row]
+        let data = albums[indexPath.row]
         controller.setup(title: data.title, predicate: data.predicate)
         navigationController?.pushViewController(controller, animated: true)
     }
